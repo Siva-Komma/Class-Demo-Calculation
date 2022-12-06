69851c852b93bb44e0c5ab50a1e0386d077c981a
Siva-Komma <kommasivajyothi22@gmail.com>
2022-12-06 21:13:33 +0530

    [Siva-Komma]Adding class calculation program

---
+++ Addition/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addition
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Sum s = new Sum();
+            Console.WriteLine("enter 1st number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter 2nd number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            s.Add(num1,num2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Addition/Sum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addition
+{
+    public class Sum
+    {
+        public void Add(int num1, int num2)
+        {
+            int results = num1 + num2;
+            Console.WriteLine ("Addion of num1 and num2:"+results);
+        }
+    }
+}
